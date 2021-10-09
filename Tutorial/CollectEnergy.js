70f0004639d3b098b00cca82430d7b080d83b748
Morton <McCrossinJoseph@hotmail.com>
2021-10-09 15:26:22 +1100

    UpdateScripts

--- Tutorial/CollectEnergy.js
+++
@@ -1,7 +0,0 @@
-module.exports.loop = function () {
-    var creep = Game.creeps['Harvester1'];
-    var sources = creep.room.find(FIND_SOURCES);
-    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
-    }
-}
