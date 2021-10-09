386bd3f573bf7760d36bfe0a8022abc8a3284347
Morton <McCrossinJoseph@hotmail.com>
2021-10-09 14:40:15 +1100

    HarvestLoop

---
+++ Tutorial/BasicHarvestloop.js
@@ -0,0 +1,7 @@
+module.exports.loop = function () {
+    var creep = Game.creeps['Harvester1'];
+    var sources = creep.room.find(FIND_SOURCES);
+    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0]);
+    }
+}
