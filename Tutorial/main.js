b2eb01795592f9e6085401b5e31411cd86cdf6df
Morton <McCrossinJoseph@hotmail.com>
2021-10-09 16:29:57 +1100

    Update-TutorialScript

--- Tutorial/main.js
+++ Tutorial/main.js
@@ -36,6 +36,13 @@ module.exports.loop = function () {
 
     var tower = Game.getObjectById('d3e54db89467ad373a9beec3');
     if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if(closestHostile) {
             tower.attack(closestHostile);
