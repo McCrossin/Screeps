fc4175f2183cca8538735a3733e0ae213d57bf91
Morton <McCrossinJoseph@hotmail.com>
2021-10-10 00:21:09 +1100

    Added conditional to allow for spawning different types of creeps depending on total energy.

--- Morton_Scripts/SpawnCreeps.js
+++ Morton_Scripts/SpawnCreeps.js
@@ -1,28 +1,60 @@
 var SpawnCreeps = {
 
-    /** @param {SpawnName} SpawnName **/
-    run: function(SpawnName) {
-	    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-        console.log('Harvesters: ' + harvesters.length);
+    /** @param {SpawnName} SpawnName 
+        {energyCapacity} Max energy**/
+    smallCreeps: function(SpawnName) {
+        var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+        //console.log('Harvesters: ' + harvesters.length);
 
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-        console.log('Upgraders: ' + upgraders.length);
+        //console.log('Upgraders: ' + upgraders.length);
 
         var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-        console.log('Builders: ' + builders.length);
-        
-        
+        //console.log('Builders: ' + builders.length);
+
         if(harvesters.length < 2) {
             var newHarvesterName = 'Harvester' + Game.time;
             console.log('Spawning new harvester: ' + newHarvesterName);
             Game.spawns[SpawnName].spawnCreep([WORK,CARRY,MOVE], newHarvesterName, 
                 {memory: {role: 'harvester'}});        
-        }else if(upgraders.length < 2){
+        }else if(upgraders.length < 3){
             var newUpgraderName = 'Upgrader' + Game.time;
             console.log('Spawning new upgrader: ' + newUpgraderName);
             Game.spawns[SpawnName].spawnCreep([WORK,CARRY,MOVE], newUpgraderName,
                 {memory: {role: 'upgrader'}})
-        }else if(builders.length < 0){
+        }else if(builders.length < 1){
+            var newBuilderName = 'Builder' + Game.time;
+            console.log('Spawning new buidler: ' + newBuilderName);
+            Game.spawns[SpawnName].spawnCreep([WORK,CARRY,MOVE], newBuilderName,
+                {memory: {role: 'builder'}})
+        }
+        
+        
+
+	},
+
+    mediumCreeps: function(SpawnName) {
+
+        var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+        //console.log('Harvesters: ' + harvesters.length);
+
+        var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+        //console.log('Upgraders: ' + upgraders.length);
+
+        var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+        //console.log('Builders: ' + builders.length);
+
+        if(harvesters.length < 2) {
+            var newHarvesterName = 'Harvester' + Game.time;
+            console.log('Spawning new harvester: ' + newHarvesterName);
+            Game.spawns[SpawnName].spawnCreep([WORK,CARRY,MOVE], newHarvesterName, 
+                {memory: {role: 'harvester'}});        
+        }else if(upgraders.length < 3){
+            var newUpgraderName = 'Upgrader' + Game.time;
+            console.log('Spawning new upgrader: ' + newUpgraderName);
+            Game.spawns[SpawnName].spawnCreep([WORK,CARRY,MOVE], newUpgraderName,
+                {memory: {role: 'upgrader'}})
+        }else if(builders.length < 1){
             var newBuilderName = 'Builder' + Game.time;
             console.log('Spawning new buidler: ' + newBuilderName);
             Game.spawns[SpawnName].spawnCreep([WORK,CARRY,MOVE], newBuilderName,
--- Morton_Scripts/main.js
+++ Morton_Scripts/main.js
@@ -2,7 +2,7 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var SpawnCreeps = require('SpawnCreeps');
-var ExpectedNumCreeps = 4
+var ExpectedNumCreeps = 6
 
 module.exports.loop = function () {
 
@@ -17,14 +17,33 @@ module.exports.loop = function () {
 
     for(var name in Game.rooms) {
         var energyAvailable = Game.rooms[name].energyAvailable;
-        console.log('Room "'+name+'" has '+ energyAvailable +' energy');
+        var energyCapacity = Game.rooms[name].energyCapacityAvailable;
+        console.log('Room "'+name+'" has '+ energyAvailable + '/' + energyCapacity + ' energy');
         
         for(const i in Game.spawns) {
-            if((energyAvailable >= 200) && (NumCreeps < ExpectedNumCreeps)){
-                SpawnCreeps.run(i);
+
+            if(energyCapacity < 600){
+                if((energyAvailable >= 200) && (NumCreeps < ExpectedNumCreeps)){
+                    SpawnCreeps.smallCreeps(i);
+                }
+            }else if(energyCapacity < 1200){
+
+                if((energyAvailable >= 600) && (NumCreeps < ExpectedNumCreeps)){
+                    SpawnCreeps.mediumCreeps(i);
+                }
             }
+            
+            if(Game.spawns[i].spawning) { 
+                var spawningCreep = Game.creeps[Game.spawns[i].spawning.name];
+                Game.spawns[i].room.visual.text(
+                    '🛠️' + spawningCreep.memory.role,
+                    Game.spawns[i].pos.x + 1, 
+                    Game.spawns[i].pos.y, 
+                    {align: 'left', opacity: 0.8});
+            }
+            
         }
-    }
+    }    
     
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
@@ -38,5 +57,6 @@ module.exports.loop = function () {
             roleBuilder.run(creep);
         }
     }
+
 }
 
