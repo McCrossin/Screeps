0ef778886f68cd05979668ceeb6114cf99ef43d3
Morton <McCrossinJoseph@hotmail.com>
2021-10-10 14:41:45 +1100

    Adding upgrader.ts

---
+++ TypeScript/src/roles/upgrader.ts
@@ -0,0 +1,13 @@
+export function roleUpgrader(creep:Creep) {
+    if(creep.store[RESOURCE_ENERGY] == 0) {
+        var sources = creep.room.find(FIND_SOURCES);
+        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[0]);
+        }
+    }
+    else {
+        if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(creep.room.controller);
+        }
+    }
+}
