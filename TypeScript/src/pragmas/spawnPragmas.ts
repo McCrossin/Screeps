b84f8ce95e9e99a8881eec4f03f7bd42522b96fc
Morton <McCrossinJoseph@hotmail.com>
2021-11-21 22:37:13 +1100

    UI added and priority fixed

--- TypeScript/src/pragmas/spawnPragmas.ts
+++ TypeScript/src/pragmas/spawnPragmas.ts
@@ -2,8 +2,8 @@ import { byId } from "selectors/byId";
 import { PriorityPragmas } from "./initPragma";
 
 export function spawnPragmas(){
-    for (let o of PriorityPragmas){
-        o.assigned = o.assigned.filter(c => byId(c)?.memory.Pragma === o.id)
-        o.spawn()
+    for (let i of PriorityPragmas){
+        i.assigned = i.assigned.filter(c => byId(c)?.memory.Pragma === i.id)
+        i.spawn()
     }
 }
