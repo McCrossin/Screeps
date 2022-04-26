3a1d6be4576bccaf7173483faa78512b4be1037c
Razmo99 <3089087+Razmo99@users.noreply.github.com>
2022-04-26 18:17:06 +1000

    comments

--- TypeScript/src/selectors/byId.ts
+++ TypeScript/src/selectors/byId.ts
@@ -1,3 +1,12 @@
+
+/**
+ * ById find a game object by a game object id
+ * @template T the type of game object Source etc
+ * @param id ID of the game object
+ * @returns the gameobject or undefined
+ */
 export function byId<T extends _HasId>(id: Id<T>|undefined){
     return id ? Game.getObjectById(id) ?? undefined : undefined
 }
+
+byId()
