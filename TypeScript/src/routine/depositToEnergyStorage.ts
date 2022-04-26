5fb3b6d1781d78b6afe86a430c122e3579f19094
Razmo99 <3089087+Razmo99@users.noreply.github.com>
2022-04-26 19:20:44 +1000

    comments

---
+++ TypeScript/src/routine/depositToEnergyStorage.ts
@@ -0,0 +1,29 @@
+import { setState, States } from "./states";
+
+
+/**
+ * Deposits to energy storage
+ * gets a creep to move to and deposit energy its holding till empty
+ * @param creep creep with energy to deposit
+ */
+export function depositToEnergyStorage(creep:Creep){
+    //Get potential storage targets
+    var targets = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+            return (structure.structureType == STRUCTURE_EXTENSION ||
+                    structure.structureType == STRUCTURE_SPAWN ||
+                    structure.structureType == STRUCTURE_TOWER) && 
+                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+        }
+    });
+    // if we have storage targets move to them and transfer
+    if(targets.length > 0) {
+        let transfer = creep.transfer(targets[0], RESOURCE_ENERGY)
+        
+        if( transfer== ERR_NOT_IN_RANGE) {
+        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+        }
+        
+        if(transfer == ERR_NOT_ENOUGH_ENERGY) setState(States.GET_ENERGY)(creep);
+    }
+}
--- TypeScript/src/routine/getEnergyFromSource.ts
+++ TypeScript/src/routine/getEnergyFromSource.ts
@@ -1,58 +1,87 @@
 import { Pragmas } from "pragmas/pragma";
+import { byId } from "selectors/byId";
 import { routineResult } from "./routineResult";
 
 declare global {
+
+    /**
+     * Creep memory
+     * property to specify the energy source target for the creep
+     */
     interface CreepMemory {
         energySource?: Id<Source>
     }
 }
 
+
+
+/**
+ * Gets energy from source
+ * @param creep creep performing the action
+ * @param OwnedRoom The ID of the room the source is in
+ * @param [sourceId] the id of the energy source
+ * @returns a routine success or failure
+ */
 export function getEnergyFromSource(creep:Creep,OwnedRoom:string,sourceId?:Id<Source>){
+    
+    // Set the creeps target if specified as a variable
     if(!creep.memory.energySource){
-        creep.memory.energySource = sourceId
+        if(byId(sourceId) != undefined) creep.memory.energySource = sourceId
     }
 
     let energySourceTarget = Game.getObjectById(creep.memory.energySource as Id<Source>)
     
+    // if the source has no energy unset it as a target and return
     if (energySourceTarget?.energy === 0){
         delete creep.memory.energySource
         return routineResult.FAILURE
     }
-
-
+    // find an energy source for the creep if it doesnt have one assigned
     if(!creep.memory.energySource){
         let room  = Game.rooms[OwnedRoom]
         if (room){
-            let [source1,source2] = room.memory.sources.map(sources =>{
+
+            let [source1,source2] = room.memory.sources.map(sources =>
+                {
+                //check we can get the source object
                 let sourceId = sources.id as Id<Source>
                 let sourceObj = Game.getObjectById(sourceId)
                 if(!sourceObj) return;
-                return{pos: sourceObj?.pos,sourceId,source:sourceObj};})
-                .filter(s => !s?.source || (s.source.energy > 0))
-            if(!source1) return routineResult.FAILURE
 
+                // return the position info of the source object
+                return{pos: sourceObj?.pos,sourceId,source:sourceObj};
+                
+            // filter results make sure its a source and has energy    
+            }).filter(s => !s?.source || (s.source.energy > 0))
+            
+
+            if(!source1) return routineResult.FAILURE
+            // Set the creep energy source to the source that has the closed linear range
+            // assuming their is 2 sources to choose from
             creep.memory.energySource = (
-                !source2 ||
-                (source1.pos?.getRangeTo(creep.pos) ?? 0) < (source2.pos?.getRangeTo(creep.pos) ?? 0)
+            !source2 ||
+            (source1.pos?.getRangeTo(creep.pos) ?? 0) < (source2.pos?.getRangeTo(creep.pos) ?? 0)
             ) ? source1.sourceId : source2.sourceId
         }
     }
 
+    // expected to have an energy source assigned by now
     if(!creep.memory.energySource) return routineResult.FAILURE
+
 
     let source = Game.getObjectById(creep.memory.energySource)
     if (!source) return routineResult.FAILURE
+    // Move to within range of the Source and mine it
     if (creep.moveTo(source.pos) == OK){
         if (creep.harvest(source) == ERR_NOT_IN_RANGE || OK){
             return routineResult.INPROGRESS
         }
     }
-
+    // harvest till capacity is free then return success and forget this energy source
     if(creep.store.getFreeCapacity(RESOURCE_ENERGY) === 0){
         delete creep.memory.energySource
         return routineResult.SUCCESS
     }else{
         return routineResult.INPROGRESS
     }
-
 }
