e932be822ec81385ca7842450717030569788fbc
Morton <example@example.com>
2022-04-28 19:10:51 +1000

    Fix Withdraw function

--- TypeScript/src/routine/withdrawFromEnergyStorage.ts
+++ TypeScript/src/routine/withdrawFromEnergyStorage.ts
@@ -11,9 +11,8 @@ export function withdrawFromEnergyStorage(creep:Creep){
     var targets = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
             return (structure.structureType == STRUCTURE_EXTENSION ||
-                    structure.structureType == STRUCTURE_SPAWN ||
-                    structure.structureType == STRUCTURE_TOWER) && 
-                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    structure.structureType == STRUCTURE_SPAWN) && 
+                    structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0;
         }
     });
     // if we have storage targets move to them and transfer
